c19666697f3029669619d180056828e97e55634b
lampham789 <Lam.Pham@ucop.edu>
2024-08-08 15:02:26 -0700

    Fix var interpolation in docker compose

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from .routes import blueprint
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-
+    
     # Set up logging
     app.logger.setLevel(app.config["LOG_LEVEL"])
     
